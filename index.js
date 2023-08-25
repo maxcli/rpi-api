1aa2a7f14a5a3339db89e116506d33d6aba3524b
peter clift <petercli@hotmail.com>
2023-08-25 02:19:54 -0700

    add post

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const app=require('express')();
+const express=require('express');
+const app=express()
 const PORT=8080;
+
+//add json middleware  to read req.body
+app.use(express.json())   
+
 app.listen(
   PORT,
 	() => console.log(`it's alive on http://localhost:${PORT}`)
@@ -7,5 +12,22 @@ app.listen(
 );
 
 app.get('/tshirt', (req, res) => {
-  res.status(200).send({"size":"large"})
+  res.status(200).send({
+    //tshirt:'\251',
+    tshirt:'\u2665',
+  
+    size:"large"
+  })
+})
+
+app.post('/tshirt/:id', (req, res) => {
+  const{id} =req.params
+  const{logo} =req.body
+  if(!logo){
+      res.status(418).send({message:'Need a logo!'})
+  }
+  res.send({
+    tshirt:` with your${logo} and ID ${id}`
+  })
+
 })
